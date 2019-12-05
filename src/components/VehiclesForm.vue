2b2fdffc443cecab3743eb792c731f9e9c96263f
Samuel Skinner <samuelskinner1998@gmail.com>
2019-12-04 20:07:30 -0500

    Some VehicleForm changes

--- src/components/VehiclesForm.vue
+++ src/components/VehiclesForm.vue
@@ -4,56 +4,100 @@
       <span class="headline">Add Vehicle</span>
     </v-card-title>
     <v-card-text>
-      <form>
-        
-      </form>
+      <form></form>
     </v-card-text>
 
-    <v-text-field
-      v-model="newVehicle.make"
-      v-bind:rules="rules.required"
-      label="Make"
-    ></v-text-field>
-    <v-text-field
-      v-model="newVehicle.model"
-      v-bind:rules="rules.required"
-      label="Model"
-    ></v-text-field>
-    <v-text-field
-      v-model="newVehicle.color"
-      v-bind:rules="rules.required"
-      label="Color"
-    ></v-text-field>
-    <!-- vehicle type dropdown -->
-    <v-text-field
-      v-model="newVehicle.capacity"
-      v-bind:rules="rules.required"
-      label="Capacity"
-    ></v-text-field>
-    <v-text-field
-      v-model="newVehicle.mpg"
-      v-bind:rules="rules.required"
-      label="MPG"
-    ></v-text-field>
-    <!-- state dropdown -->
-    <v-text-field
-      v-model="newVehicle.license_number"
-      v-bind:rules="rules.required"
-      label="License Number"
-    ></v-text-field>
-    
-    <v-card-actions>  
-      <v-btn color="secondary" flat :disabled="saving" v-on:click="cancel">Cancel</v-btn>
+    <v-row align="center">
+      <v-col col="12" sm="6">
+        <v-text-field
+          v-model="newVehicle.make"
+          v-bind:rules="rules.required"
+          label="Make"
+        ></v-text-field>
+      </v-col>
+      <v-col col="12" sm="6">
+        <v-text-field
+          v-model="newVehicle.model"
+          v-bind:rules="rules.required"
+          label="Model"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row align="center">
+      <v-col col="12" sm="6">
+        <v-text-field
+          v-model="newVehicle.color"
+          v-bind:rules="rules.required"
+          label="Color"
+        ></v-text-field>
+      </v-col>
+      <v-col col="12" sm="6">
+        <v-select 
+          label="Vehicle Type" 
+          v-model="newVehicle.vehicle_type_id"
+          v-bind:rules="rules.required"
+          :items="typesList"
+          item-text="type"
+        ></v-select>
+      </v-col>
+    </v-row>
+
+    <v-row align="center">
+      <v-col col="12" sm="6">
+        <v-text-field
+          v-model="newVehicle.capacity"
+          v-bind:rules="rules.required"
+          label="Capacity"
+        ></v-text-field>
+      </v-col>
+      <v-col col="12" sm="6">
+        <v-text-field
+          v-model="newVehicle.mpg"
+          v-bind:rules="rules.required"
+          label="MPG"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row align="center">
+      <v-col col="12" sm="6">
+        <v-select 
+          label="License State" 
+          v-model="newVehicle.license_state"
+          v-bind:rules="rules.required"
+          :items="statesList"
+          item-text="name"
+        ></v-select>
+      </v-col>
+      <v-col col="12" sm="6">
+        <v-text-field
+          v-model="newVehicle.license_number"
+          v-bind:rules="rules.required"
+          label="License Number"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-card-actions>
+      <v-btn 
+        color="secondary" 
+        :disabled="saving" 
+        v-on:click="cancel"
+      >Cancel</v-btn>
       <v-spacer></v-spacer>
-      <v-btn color="primary" flat :disabled="saving" v-on:click="handleClear">Clear</v-btn>
+      <v-btn 
+        color="primary" 
+        :disabled="saving" 
+        v-on:click="handleClear"
+      >Clear</v-btn>
       <v-btn
         color="primary"
         raised
         :disabled="saving"
         :loading="saving"
         v-on:click="handleSave"
-        >Save</v-btn
-      >
+      >Save</v-btn>
     </v-card-actions>
 
     <div class="text-xs-center">
@@ -76,7 +120,6 @@
         </v-card>
       </v-dialog>
     </div>
-
   </v-card>
 </template>
 
@@ -97,6 +140,8 @@ export default {
         license_state: "",
         license_number: ""
       },
+      typesList: [],
+      statesList: [],
 
       vehicleCreated: false,
 
@@ -109,6 +154,17 @@ export default {
       }
     };
   },
+  beforeMount: function() {
+    console.log("hi");
+    this.$axios.get("/vehicle-types").then(response => {
+      response.data.map(t => this.typesList.push(t));
+    });
+    console.log(this.typesList);
+    this.$axios.get("/states").then(response => {
+      response.data.map(s => this.statesList.push(s));
+    });
+    console.log(this.statesList);
+  },
   methods: {
     handleClear: function() {
       this.newVehicle.make = "";
@@ -117,6 +173,8 @@ export default {
       this.newVehicle.capacity = "";
       this.newVehicle.mpg = "";
       this.newVehicle.license_number = "";
+      this.newVehicle.vehicle_type_id = "";
+      this.newVehicle.license_state = "";
     },
     handleSave: function() {
       this.vehicleCreated = false;
@@ -135,7 +193,7 @@ export default {
           if (result.status == 200) {
             if (result.data.ok) {
               this.showDialog("Success", result.data.msge);
-              this.vehicleCreated = true; 
+              this.vehicleCreated = true;
             } else {
               this.showDialog("Failed", result.data.msge);
             }
@@ -151,7 +209,7 @@ export default {
     hideDialog: function() {
       this.dialogVisible = false;
       if (this.vehicleCreated) {
-        this.$router.push({name: "vehicles"});
+        this.$router.push({ name: "vehicles" });
       }
     }
   }
